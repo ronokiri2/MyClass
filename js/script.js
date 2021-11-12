6472f3aed532dea2c793c7204ad1cd3cc6eca010
Kirill Romanov <58993148+ronokiri2@users.noreply.github.com>
2021-11-12 22:18:05 +0300

    refactoring 7 setClass
    Добавим функцию setClass (установить класс). Селектор на целевой элемент также получаем из свойства settingTarget. Для установки класса используем метод classList.add(value).

--- js/script.js
+++ js/script.js
@@ -8,10 +8,18 @@ const setDataAttribute = ({settingTarget}, params) => {
       element.dataset[key] = value;
     }
   };
+
+const setClass = ({settingTarget}, params) => {
+    const element = document.querySelector(settingTarget);
+
+    for (const [key, value] of Object.entries(params)) {
+        element.classList.add(value);
+    }
+};
   
 const applySetting = (setting, params) => {
     if (setting.settingType === 'class') {
-  
+        setClass(setting, params);
     } else if (setting.settingType === 'attribute') {
         setDataAttribute(setting, params);
     }
@@ -29,7 +37,7 @@ const settingButtonClickHandler = (evt, setting) => {
   const settingName = button.dataset.settingName;
   const settingValue = button.dataset.settingValue;
   params[settingName] = settingValue;
-
+  
   applySetting(setting, params);
 };
 
